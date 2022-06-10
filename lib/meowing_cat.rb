053eb92fe996800dd4d101a13753d0403450a5be
MissDine <enidnyatichi1212@gmail.com>
2022-06-10 15:07:25 +0300

    Feature edit

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,5 @@
-## code your solution here. 
+## code your solution here.
+# metaprogramming ot the implementation of Macro is the writing of programmes that operate on other programmes 
+# Metaprogramming can make our lives easier by automating repetitive tasks or providing something like a mini-language inside of another programming language that has the benefits of being concise and descriptive.
+# It is dangerous in the sense that it can result in very hard to follow code that obscures what is actually happening.
+
